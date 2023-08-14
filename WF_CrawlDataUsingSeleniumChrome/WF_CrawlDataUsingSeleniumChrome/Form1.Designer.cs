36695aeadde54456f4753905d57cc939e376f386
HoàngPQ <32738245+quanghoangvhitc@users.noreply.github.com>
2023-08-14 17:16:01 +0700

    Add files via upload

---
+++ WF_CrawlDataUsingSeleniumChrome/WF_CrawlDataUsingSeleniumChrome/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WF_CrawlDataUsingSeleniumChrome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.lbData = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(13, 13);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // lbData
+            // 
+            this.lbData.FormattingEnabled = true;
+            this.lbData.Location = new System.Drawing.Point(95, 13);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(693, 420);
+            this.lbData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbData);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.ListBox lbData;
+    }
+}
+
---
+++ WF_CrawlDataUsingSeleniumChrome/WF_CrawlDataUsingSeleniumChrome/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WF_CrawlDataUsingSeleniumChrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public async Task<List<string>> GetData()
+        {
+            
+            string fullUrl = "https://investorlift.com/";
+            List<string> programmerLinks = new List<string>();
+
+            try
+            {
+                var options = new ChromeOptions()
+                {
+                    BinaryLocation = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+                };
+
+                options.AddArguments("--proxy-server=" + "172.16.23.254" + ":" + "3128"); // sử dụng qua proxy
+                options.AddArguments(new List<string>() { "headless", "disable-gpu" }); // không hiện trang chrome lên
+
+                IWebDriver browser = new ChromeDriver(options);
+                browser.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(5);
+                browser.Navigate().GoToUrl(fullUrl);
+
+                int a = -1;
+                int b = 0;
+
+                while (a < b)
+                {
+                    ((IJavaScriptExecutor)browser).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 150)");
+                    a = b;
+                    await Task.Delay(5).ContinueWith(t =>
+                    {
+                        //var webElements = browser.FindElements(By.ClassName("listing-compact-title"));
+                        var webElements = browser.FindElements(By.ClassName("property-list-item"));
+                        b = webElements.Count;
+                    });
+                }
+
+                //var links = browser.FindElementsByXPath("//li[not(contains(@class, 'tocsection'))]/a[1]");
+                var elements = browser.FindElements(By.ClassName("property-list-item"));
+                int i = 1;
+                foreach (var element in elements)
+                {
+                    var url = element.FindElement(By.ClassName("listing-item"));
+                    var title = element.FindElement(By.ClassName("listing-compact-title"));
+                    var address = element.FindElement(By.ClassName("second-line-address"));
+                    var price = element.FindElement(By.ClassName("listing-price"));
+
+                    programmerLinks.Add($"{i}: Url: {url.GetAttribute("href")}      Title: {title.Text}     Address: {address.Text}     Price: {price.Text}");
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"GetData ERR: {ex.ToString()}");
+            }
+
+            return programmerLinks;
+        }
+
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var datas = await GetData();
+            Console.WriteLine($"Crawl completed: {datas.Count} item - {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Stop();
+            if (datas != null)
+            {
+                lbData.DataSource = datas;
+            }    
+        }
+    }
+}
